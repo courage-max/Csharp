5c883bc8e3b6ef975e602a5589a6b72efc3b1e7a
seoyeong7.choi@gmail.com <seoyeong7.choi@gmail.com>
2020-07-31 15:20:19 +0900

    First commit

---
+++ MvcMovie/MvcMovie/App_Start/RouteConfig.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace MvcMovie
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Default", // 이름
+                url: "{controller}/{action}/{id}", // URL 패턴
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional } // 기본 경로 값
+            );
+
+            routes.MapRoute(
+                name: "Hello",
+                url: "{controller}/{action}/{name}/{id}"
+            );
+        }
+    }
+}
---
+++ MvcMovie/MvcMovie/Models/Movie.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Data.Entity;
+
+namespace MvcMovie.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; }
+        public decimal Price { get; set; }
+    }
+
+    public class MovieDBContext : DbContext // Entity Framework
+    {
+        // DB명: MovieDBContext => Web.config 에서 connectionStrings 내의 add name과 동일해야 함
+        public DbSet<Movie> Movies { get; set; } // Movie table
+    }
+}
