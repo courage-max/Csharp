526ec7d52622aaa187482bb551e1ddbe1b798941
seoyeong7.choi@gmail.com <seoyeong7.choi@gmail.com>
2020-07-31 18:05:42 +0900

    Fix MVC

---
+++ MvcMovie/MvcMovie/Controllers/MoviesController.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MvcMovie.Models;
+
+namespace MvcMovie.Controllers
+{
+    public class MoviesController : Controller
+    {
+        private MovieDBContext db = new MovieDBContext();
+
+        // GET: Movies
+        public ActionResult Index(string movieGenre, string searchString)
+        {
+            var GenreLst = new List<string>();
+
+            var GenreQry = from d in db.Movies
+                           orderby d.Genre
+                           select d.Genre; // 장르 검색 LINQ 쿼리
+
+            GenreLst.AddRange(GenreQry.Distinct());
+            ViewBag.movieGenre = new SelectList(GenreLst, "Comedy"); 
+            // null 하거나 필터링 하지 않은 단계라 빈 값 제출시 모든 장르 표시, 위는 Comedy를 기본 옵션으로 사용할 경우
+
+            var movies = from m in db.Movies
+                         select m;
+
+            if (!String.IsNullOrEmpty(searchString)) // Null 체크
+            {
+                movies = movies.Where(s => s.Title.Contains(searchString)); // 포함하는지 체크
+            }
+
+            if (!string.IsNullOrEmpty(movieGenre)) // Null 체크
+            {
+                movies = movies.Where(x => x.Genre == movieGenre);
+            }
+
+            return View(movies);
+        }
+
+        // GET: Movies/Details/5
+        public ActionResult Details(int? id) // null check
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Movie movie = db.Movies.Find(id);
+            if (movie == null)
+            {
+                return HttpNotFound();
+            }
+            return View(movie);
+        }
+
+        // GET: Movies/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Movies/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Title,ReleaseDate,Genre,Price")] Movie movie)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Movies.Add(movie);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(movie);
+        }
+
+        // GET: Movies/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Movie movie = db.Movies.Find(id);
+            if (movie == null)
+            {
+                return HttpNotFound();
+            }
+            return View(movie);
+        }
+
+        // POST: Movies/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken] // 요청 위조 방지(토근의 유효성 검사) 어노테이션
+        public ActionResult Edit([Bind(Include = "ID,Title,ReleaseDate,Genre,Price")] Movie movie)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(movie).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(movie);
+        }
+
+        // GET: Movies/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Movie movie = db.Movies.Find(id);
+            if (movie == null)
+            {
+                return HttpNotFound();
+            }
+            return View(movie);
+        }
+
+        // POST: Movies/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Movie movie = db.Movies.Find(id);
+            db.Movies.Remove(movie);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ MvcMovie/MvcMovie/Migrations/Configuration.cs
@@ -0,0 +1,59 @@
+﻿namespace MvcMovie.Migrations
+{
+    using MvcMovie.Models;
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<MvcMovie.Models.MovieDBContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "MvcMovie.Models.MovieDBContext";
+        }
+
+        protected override void Seed(MvcMovie.Models.MovieDBContext context)
+        {
+            // 첫 변경 후 CTRL-SHIFT-B로 빌드 => 반드시!
+            // add-migration Initial => initial migration 생성
+            // {DateStamp}_Initial.cs Class가 만들어지는데 schema도 포함하여 만들어준다
+            // update-database 는 DB를 만들고 Seed 메소드를 실행시킨다
+            context.Movies.AddOrUpdate(i => i.Title,
+                new Movie
+                {
+                    Title = "When Harry Met Sally",
+                    ReleaseDate = DateTime.Parse("1989-1-11"),
+                    Genre = "Romantic Comedy",
+                    Price = 7.99M
+                },
+
+                 new Movie
+                 {
+                     Title = "Ghostbusters ",
+                     ReleaseDate = DateTime.Parse("1984-3-13"),
+                     Genre = "Comedy",
+                     Price = 8.99M
+                 },
+
+                 new Movie
+                 {
+                     Title = "Ghostbusters 2",
+                     ReleaseDate = DateTime.Parse("1986-2-23"),
+                     Genre = "Comedy",
+                     Price = 9.99M
+                 },
+
+               new Movie
+               {
+                   Title = "Rio Bravo",
+                   ReleaseDate = DateTime.Parse("1959-4-15"),
+                   Genre = "Western",
+                   Price = 3.99M
+               }
+           );
+
+        }
+    }
+}
